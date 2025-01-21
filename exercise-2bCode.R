d9150ef5726f26fb4a27b5a8e995cd29c9029aba
NicoJaws23 <nico.jaworski@utexas.edu>
2025-01-21 14:40:31 -0600

    Adding Code saved to project

---
+++ exercise-2bCode.R
@@ -0,0 +1,6 @@
+# uncomment the following line, if needed, to install the {usethis} package
+# install.packages('usethis')
+
+library(usethis)
+use_git_config(user.name = "NicoJaws23", user.email = "nico.jaworski@utexas.edu")
+detach(package:usethis)
